4db18783c915dde0794778e359fe4fb03c780580
Christopher Pope <christopher@thepopes.org>
2023-01-24 19:38:16 -0500

    .

---
+++ Controllers/VideoController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using Microsoft.Net.Http.Headers;
+using TatterFitness.Bll.Interfaces.Services;
+
+namespace TatterFitness.Videos.Controllers
+{
+    public class VideoController : Controller
+    {
+        private readonly IVideoService videoSvc;
+
+        public VideoController(IVideoService videoSvc)
+        {
+            this.videoSvc = videoSvc;
+        }
+
+        public FileStreamResult Index(int id)
+        {
+            var videoData = videoSvc.ReadVideoData(id);
+            var stream = new MemoryStream(videoData);
+            var contentType = new MediaTypeHeaderValue("video/mp4");
+            return new FileStreamResult(stream, contentType)
+            {
+                FileDownloadName = "workout.mp4"
+            };
+
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TatterFitness.Videos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
